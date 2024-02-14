1e43f309d1acadc52f65c72de316b692fc2b067c
Castro-1 <jecastrog@eafit.edu.co>
2024-02-14 08:59:37 -0500

    first tutorial done

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', 'Contact us - Online Store')
+@section('subtitle', 'Contact us')
+@section('content')
+<div>
+    <ul>
+        <li><b>Email:</b>micorreo@gmail.com</li>
+        <li><b>Address:</b>Cra 43 # 4-300</li>
+        <li><b>Phone:</b>3118794688</li>
+    </ul>
+</div>
+@endsection
