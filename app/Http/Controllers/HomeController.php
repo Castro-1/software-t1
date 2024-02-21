8a942089f266d432e459d4a4b04c8555416929ab
Castro-1 <jecastrog@eafit.edu.co>
2024-02-21 08:07:23 -0500

    malas prácticas corregidas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,14 +1,25 @@
 <?php
- 
+
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
- 
+
 class HomeController extends Controller
 {
     public function index(): View
     {
         return view('home.index');
+    }
+
+    public function about(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'About us - Online Store';
+        $viewData['subtitle'] = 'About us';
+        $viewData['description'] = 'This is an about page ...';
+        $viewData['author'] = 'Developed by: Juan Esteban Castro';
+
+        return view('home.about')->with('viewData', $viewData);
     }
 
     public function contact(): View
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\RedirectResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +13,10 @@ use Illuminate\Http\RedirectResponse;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
-Route::get('/about', function () {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Juan Esteban Castro";
-    return view('home.about')->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-})->name("home.about");
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
-Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name("home.contact");
-
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
+Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
