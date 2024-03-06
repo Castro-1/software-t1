b9594336323fdcec5b4783360b039de9990bb6ee
Castro-1 <jecastrog@eafit.edu.co>
2024-03-06 08:00:41 -0500

    fix: formateador de blade y sesión

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -2,11 +2,11 @@
 @section('title', 'Contact us - Online Store')
 @section('subtitle', 'Contact us')
 @section('content')
-<div>
-    <ul>
-        <li><b>Email:</b>micorreo@gmail.com</li>
-        <li><b>Address:</b>Cra 43 # 4-300</li>
-        <li><b>Phone:</b>3118794688</li>
-    </ul>
-</div>
+    <div>
+        <ul>
+            <li><b>Email:</b>micorreo@gmail.com</li>
+            <li><b>Address:</b>Cra 43 # 4-300</li>
+            <li><b>Phone:</b>3118794688</li>
+        </ul>
+    </div>
 @endsection
